cb6b6c0e205c23b57926ffe6f4c7976ff0e3f761
Anton Viktorovich <75506917+AntonViktorovichD@users.noreply.github.com>
2023-02-02 10:21:06 +0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from ftplib import FTP
+from pathlib import Path
+import os
+
+ftp = FTP("77.222.57.4")
+ftp.login("grandnn201_grandnn", "M8B^4QXHZNLZm2NW")
+for item in [w for w in Path('text.txt').read_text(encoding="utf-8").replace("\n", " ").split()]:
+    ftp.cwd(f'/{item[0]}')
+    try:
+        ftp.size(item + '.jpg')
+        with open(f'{item}.jpg', 'wb') as file:
+            ftp.retrbinary(f'RETR {item}.jpg', file.write)
+            print(item)
+    except Exception:
+        continue
+ftp.quit()
